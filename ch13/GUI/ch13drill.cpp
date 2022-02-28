d580b47946a34e6dbd9fa92e7672da15b8d03bc6
DaRainbowWarrior <onlyforsmething@gmail.com>
2022-02-28 18:34:00 +0100

    almost working

--- ch13/GUI/ch13drill.cpp
+++ ch13/GUI/ch13drill.cpp
@@ -1,5 +1,5 @@
 /*
-    g++ ch13drill.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o ch13drill `fltk-config --ldflags --use-images` -std=c++11
+    g++ ch13drill.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o ch13drill.out `fltk-config --ldflags --use-images` -std=c++11
 */
 #include "Simple_window.h"
 #include "Graph.h"
@@ -7,6 +7,8 @@
 int main()
 {
     using namespace Graph_lib;
+
+
 
     int xmax = 800;
     int ymax = 1000;
@@ -17,6 +19,7 @@ int main()
 	int x_grid = 100;
 	int y_grid = 100;
 
+	Simple_window win {Point{100,100}, xmax, ymax, "Canvas"};
 
 	Lines grid;
 	for (int x=x_grid; x<x_size; x+=x_grid)
@@ -24,20 +27,43 @@ int main()
 	for (int y = y_grid; y<y_size; y+=y_grid)
 		grid.add(Point{0,y},Point{x_size,y});
 
-	int plusz = y_size;
-	//for(int i=0; i<=x_size; i=i+x_size)
-	//{
-		Rectangle Rect {Point{plusz,plusz},100,100};
-		plusz = plusz + y_size;
-	//}
+	Vector_ref<Rectangle> rekt;
+
+	for(int i=0; i < x_size; i += x_grid)
+	{
+		rekt.push_back(new Rectangle{Point{i,i}, Point{i+x_grid,i+x_grid}});
+		rekt[rekt.size() - 1].set_fill_color(Color::red);
+		win.attach(rekt[rekt.size() - 1]);
+	}
 
 
-
-    Simple_window win {Point{100,100}, xmax, ymax, "Canvas"};
+	Image image1 {Point{0,300},"beka.jpg"};
+    Image image2 {Point{300,600},"beka.jpg"};
+    Image image3 {Point{600,300},"beka.jpg"};
+    Image imageR {Point{0,0}, "beka2.jpg"};
 
     win.attach(grid);
-    win.attach(Rect);
+	win.attach(image1);
+	win.attach(image2);
+	win.attach(image3);
+	win.attach(imageR);
 
-    win.wait_for_button();
+	win.wait_for_button();
+
+	for(int i=0;i<701;i+=100)
+	{
+		for(int j=0;j<700;j+=100)
+		{
+			imageR.move(100,0);
+			win.wait_for_button();
+		}
+		if( i < 700)
+		{
+			imageR.move(-700,100);
+			win.wait_for_button();
+		}
+	}
+
+
 
 }

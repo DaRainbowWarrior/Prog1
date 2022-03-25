8ab8c28a679780f3f36b3f16d15493e9ee66e2ea
DaRainbowWarrior <onlyforsmething@gmail.com>
2022-03-25 16:41:07 +0100

    ch16 drill done

--- ch16/GUI/Lines_window.cpp
+++ ch16/GUI/Lines_window.cpp
@@ -4,18 +4,84 @@ Lines_window::Lines_window(Point xy, int w, int h, const string& title)
     :Window{xy,w,h,title},
     next_button{Point{x_max()-300,0}, 150, 40, "Next point",
         [](Address, Address pw) {reference_to<Lines_window>(pw).next();}},
+
     quit_button{Point{x_max()-150,0}, 150, 40, "Quit",
         [](Address, Address pw) {reference_to<Lines_window>(pw).quit();}},
+
     next_x{Point{300,0}, 50, 40, "next x:"},
     next_y{Point{450,0}, 50, 40, "next y:"},
-    xy_out{Point{100,0}, 100, 40, "current (x,y):"}
+    xy_out{Point{100,0}, 100, 40, "current (x,y):"},
+
+    color_menu{Point{x_max()-150,40},150,40,Menu::vertical,"color"},
+    colormenu_button{Point{x_max()-150,40}, 150, 40, "color menu", cb_colormenu},
+
+    line_menu{Point{x_max()-300,40},150,40,Menu::vertical,"line"},
+    linemenu_button{Point{x_max()-300,40}, 150, 40, "line menu", cb_line_menu}
+
 {
     attach(next_button);
     attach(quit_button);
     attach(next_x);
     attach(next_y);
     attach(xy_out);
+    xy_out.put("no point");
+    
+
+    color_menu.attach(new Button{Point{0,0},0,0,"red",cb_red});
+    color_menu.attach(new Button{Point{0,0},0,0,"blue",cb_blue});
+    color_menu.attach(new Button{Point{0,0},0,0,"black",cb_black});
+    attach(color_menu);
+    color_menu.hide();
+    attach(colormenu_button);
+
+    line_menu.attach(new Button{Point{0,0},0,0,"solid",cb_solid});
+    line_menu.attach(new Button{Point{0,0},0,0,"dashed",cb_dashed});
+    line_menu.attach(new Button{Point{0,0},0,0,"dotted",cb_dotted});
+    attach(line_menu);
+    line_menu.hide();
+    attach(linemenu_button);
+
     attach(lines);
+}
+
+void Lines_window::cb_red(Address, Address pw)
+{
+    reference_to<Lines_window>(pw).red_pressed();
+}
+
+void Lines_window::cb_blue(Address, Address pw)
+{
+    reference_to<Lines_window>(pw).blue_pressed();
+}
+
+void Lines_window::cb_black(Address, Address pw)
+{
+    reference_to<Lines_window>(pw).black_pressed();
+}
+
+void Lines_window::cb_colormenu(Address, Address pw)
+{
+    reference_to<Lines_window>(pw).colormenu_pressed();
+}
+
+void Lines_window::cb_solid(Address, Address pw)
+{
+    reference_to<Lines_window>(pw).solid_pressed();
+}
+
+void Lines_window::cb_dashed(Address, Address pw)
+{
+    reference_to<Lines_window>(pw).dashed_pressed();
+}
+
+void Lines_window::cb_dotted(Address, Address pw)
+{
+    reference_to<Lines_window>(pw).dotted_pressed();
+}
+
+void Lines_window::cb_line_menu(Address, Address pw)
+{
+    reference_to<Lines_window>(pw).linemenu_pressed();
 }
 
 void Lines_window::quit()
--- ch16/GUI/Lines_window.h
+++ ch16/GUI/Lines_window.h
@@ -4,9 +4,13 @@ using namespace Graph_lib;
 
 struct Lines_window : Graph_lib::Window {
 	Lines_window(Point xy, int w, int h, const string& title );
+	
 	Open_polyline lines;
-
+	
 	bool wait_for_button();
+
+	
+
 
 private:
 	Button next_button;
@@ -16,8 +20,39 @@ private:
 	In_box next_y;
 
 	Out_box xy_out;
+	
+	Menu color_menu;
+	Button colormenu_button;
+
+	Menu line_menu;
+	Button linemenu_button;
+
+	void changecolor(Color c) { lines.set_color(c); }
+	void hide_colormenu() { color_menu.hide(); colormenu_button.show(); }
+
+	void changeline(Line_style s){lines.set_style(s);}
+    void hide_linemenu() {line_menu.hide(); linemenu_button.show(); }
+
+	void red_pressed() { changecolor(Color::red); hide_colormenu(); }
+	void blue_pressed() { changecolor(Color::blue); hide_colormenu(); }
+	void black_pressed() { changecolor(Color::black); hide_colormenu(); }
+	void colormenu_pressed() { colormenu_button.hide(); color_menu.show(); }
+	
+	void dotted_pressed() { changeline(Line_style::dot); hide_linemenu(); }
+    void dashed_pressed() { changeline(Line_style::dash); hide_linemenu(); }
+    void solid_pressed() { changeline(Line_style::solid); hide_linemenu(); }
+
+    void linemenu_pressed() { linemenu_button.hide(); line_menu.show(); }
 
 	void next();
 	void quit();
-
+	
+	static void cb_red(Address, Address);
+	static void cb_blue(Address, Address);
+	static void cb_black(Address, Address);
+	static void cb_colormenu(Address, Address);
+	static void cb_solid(Address,Address);
+    static void cb_dashed(Address,Address);
+    static void cb_dotted(Address,Address);
+    static void cb_line_menu(Address, Address);
 };
